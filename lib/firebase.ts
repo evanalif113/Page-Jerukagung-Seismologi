0df6840d5954e9b0820b14148571dac72ebd6ddc
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-07-06 15:12:29 +0700

    Implement code changes to enhance functionality and improve performance

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,22 @@
 import { initializeApp } from "firebase/app"
 import { getDatabase, ref, query, orderByKey, limitToLast, get, onValue, off, startAt, endAt} from "firebase/database"
+import { Auth, getAuth } from "firebase/auth"
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDalcCwwOthPMjC3umkpQECqlQQj699FTY",
-  authDomain: "staklimjerukagung.firebaseapp.com",
-  databaseURL: "https://staklimjerukagung-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "staklimjerukagung",
-  storageBucket: "staklimjerukagung.appspot.com",
-  messagingSenderId: "763003005982",
-  appId: "1:763003005982:web:8ce295eda92c6b9112d20f",
-  measurementId: "G-DRL05TMRNT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
+const auth = getAuth(app)
 
 export { database, ref, query, orderByKey, limitToLast, get, onValue, off, startAt, endAt }
